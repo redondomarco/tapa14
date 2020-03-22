40c4723cd61125d0fb305fda01de59b6a362cd10
Marco Redondo <redondomarco@gmail.com>
2020-03-22 01:38:27 -0300

    fix mensajes de instalador

--- tapa14-install.sh
+++ tapa14-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo instalo paquetes sistema operativo -> base de datos incluida !
+echo instalo paquetes sistema operativo - base de datos incluida !
 sudo apt-get install -y build-essential libpoppler-cpp-dev pkg-config python3-dev python3-pip swig libssl-dev git unzip ipython3 python3-tk postgresql libpq-dev
 
 echo instalo paquetes python
@@ -15,11 +15,11 @@ echo
 cd ~/web2py/applications/tapa14
 git clone https://github.com/redondomarco/tapa14-files.git files
 echo
-echo copiar logging.conf opcional ->> por defecto a file
-echo para syslog copiar manualmente logging.conf-syslog -> apunta a local3
+echo copiar logging.conf opcional - por defecto a file
+echo para syslog copiar manualmente logging.conf-syslog apunta a local3
 echo
 cp ~/web2py/applications/tapa14/files/conf/logging/logging.conf-file ~/web2py/logging.conf
-echo copio configuracion appconfig.ini inicial -> configurar !!
+echo copio configuracion appconfig.ini inicial - configurar !!
 mkdir -p ~/web2py/applications/tapa14/private
 mkdir -p ~/web2py/applications/tapa14/databases
 cp ~/web2py/applications/tapa14/files/conf/appconfig.ini ~/web2py/applications/tapa14/private/
